f18d5219ca90f0cb6853d334c3eb203af10f8071
JuanNieto92 <jnietodelavega@gmail.com>
2022-02-04 20:18:46 -0300

    develop

--- api/index.js
+++ api/index.js
@@ -10,10 +10,7 @@ const app = express()
 app.use(bodyParser.json())
 app.use(cors())
 
-
-const url ="mongodb+srv://almuerzimaster:almuerzi22@cluster0.4xben.mongodb.net/almuerzi-db?retryWrites=true&w=majority"
 mongoose.connect(process.env.MONGO_URI,{useNewUrlParser: true, useUnifiedTopology: true})
-//mongoose.connect(url,{useNewUrlParser: true, useUnifiedTopology: true})
 
 app.use('/api/meals',meals)
 app.use('/api/orders',orders)
