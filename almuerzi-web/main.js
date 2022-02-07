8bb6cda8975e2ab21778be258da7c338cc3c969a
Juan Andrés Nieto de la Vega <jnietodelavega@hotmail.com>
2022-02-07 16:44:05 -0300

    develop

--- almuerzi-web/main.js
+++ almuerzi-web/main.js
@@ -1,5 +1,11 @@
 window.onload = () => {
   fetch('https://serverless-juannieto92.vercel.app/api/meals')
     .then(response => response.json())
-    .then(data => console.log(data))
+    .then(data => {
+      const mealsList = document.getElementById('meals-list');
+      const submit = document.getElementById('submit')
+      const template = data.map(x => '<li>'+x.name+'</li>').join('')
+      mealsList.innerHTML = template;
+      submit.removeAttribute('disabled')
+    })
 }
