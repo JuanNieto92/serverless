cc3e1c5f1749a54127c633419a218bb1fb50bd6c
Juan Andrés Nieto de la Vega <jnietodelavega@hotmail.com>
2022-02-11 14:09:09 -0300

    se agregan archivos para autorización

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 
 const meals = require('./routes/meals')
 const orders = require('./routes/orders')
+const auth = require('./routes/auth')
 
 const app = express()
 app.use(bodyParser.json())
@@ -16,6 +17,7 @@ mongoose.connect(process.env.MONGO_URI,{useNewUrlParser: true, useUnifiedTopolog
 
 app.use('/api/meals',meals)
 app.use('/api/orders',orders)
+app.use('/api/auth',auth)
 
 module.exports = app
 
